e027853bf0bff68287cf53767be4c592e91403a0
Vincent Servio <vincentservio@gmail.com>
2020-02-20 21:58:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require "pry"
+class Dog
+  @@all = []
+  
+  def initialize(name)
+   @name = name
+    @@all << self
+ end
+  
+  
+  def name
+    @name
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all 
+    @@all.clear
+  end 
+  
+  def self.print_all
+   self.all.each do |i, o| puts i.name
+
+  end
+ end
+  
+  def save
+    self.class.all << self
+  end
+end 
+
+#<Dog:0x00000000025dda70 @name="Maddy"
